ec393962848ca92681fb4a0c5cbc1d01e3734a8f
yashicanagal5-hub <yashicanagal5@gmail.com>
2025-10-31 00:40:13 +0530

    Delete excel-analytics-platform/test-env.js

--- excel-analytics-platform/test-env.js
+++
@@ -1,7 +0,0 @@
-const result = require('dotenv').config();
-if (result.error) {
-    console.log('dotenv error:', result.error);
-} else {
-    console.log('dotenv loaded successfully');
-}
-console.log('MONGO_URI:', process.env.MONGO_URI);
